c46a026765865f8ba93f27f52ebd5016629438ec
Dmytro Batarin <dmytro@batarin.dev>
2023-09-03 15:54:32 +0000

    Add dark background

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -3,7 +3,7 @@ import { UserDropdown } from "./user-dropdown.component";
 
 export function Navbar() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 dark:bg-gray-800">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
