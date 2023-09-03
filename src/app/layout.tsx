5ae408c5b7005eb316176822f9c767ef7b65e9db
Dmytro Batarin <dmytro@batarin.dev>
2023-09-03 16:22:13 +0000

    Fetch boards on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components";
 import "./globals.css";
 import type { Metadata } from "next";
+import { ReactQueryProvider } from "@/providers";
 
 export const metadata: Metadata = {
   title: "Trello clone",
@@ -15,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="dark bg-gray-900">
-        <Navbar />
-        {children}
+        <ReactQueryProvider>
+          <Navbar />
+          {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { BoardCard } from "@/components";
+import { prisma } from "@/core/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const boards = await prisma.boards.findMany();
+
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <BoardCard id="1" title="Board 1" />
-        <BoardCard id="2" title="Board 2" />
+        {boards.map((board) => (
+          <BoardCard key={board.id} id={board.id} title={board.title} />
+        ))}
       </div>
     </div>
   );
