a8b665fa6811744014bbb17fecd46a931c4e198f
Dmytro Batarin <dmytro@batarin.dev>
2023-10-29 14:29:58 +0000

    Fix loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,6 @@
-export default function Loading() {
+import { Loading } from "@/components/loading.component";
+
+export default function LoadingPage() {
   return (
     <div className="absolute bottom-0 top-26 w-full flex justify-center items-center">
       <Loading />
